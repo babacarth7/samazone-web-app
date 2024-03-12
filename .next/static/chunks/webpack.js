9cff8355ecf6f1a73aa1bfed4851bb0335bc8c0f
Babacar Thiam <todos-mop-0q@icloud.com>
2024-03-12 13:08:53 +0000

    added some files to ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30b8b7eea19c4738"; }
+/******/ 		__webpack_require__.h = function() { return "eb30336130cb1310"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
